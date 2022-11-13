13ef31bdc3c867e54d5a4ceb59c90c10783f47bb
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-11-13 21:43:46 +0200

    order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,7 +10,13 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $hidden = ['updated_at'];
+
     public function orderMeals(){
         return $this->hasMany(OrderMeal::class,'order_id');
+    }
+
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 }
