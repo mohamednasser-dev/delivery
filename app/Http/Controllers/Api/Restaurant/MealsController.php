5456d088f18e3412c82fdd38b5111d1df6f5a8b6
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-06 21:58:28 +0200

    delete item

--- app/Http/Controllers/Api/Restaurant/MealsController.php
+++ app/Http/Controllers/Api/Restaurant/MealsController.php
@@ -223,7 +223,7 @@ class MealsController extends Controller
 
     public function deleteItem(DeleteItemMealRequest $request)
     {
-        dd(request()->type);
+//        dd(request()->type);
 //        $request = $request->validated();
 
         if(request()->type == "addon"){
