0e3703ae97d91fb16a9f4ebc84e9224f88977adf
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-06 21:24:35 +0200

    delete item

--- app/Http/Controllers/Api/Restaurant/MealsController.php
+++ app/Http/Controllers/Api/Restaurant/MealsController.php
@@ -225,16 +225,16 @@ class MealsController extends Controller
     {
         $request = $request->validated();
 
-        if($request->type == "addon"){
+        if(request()->type == "addon"){
             MealAddon::whereId($request->id)
                 ->where('meal_id',$request->meal_id)->delete();
         }
-        if($request->type == "attribute"){
+        if(request()->type == "attribute"){
             MealAttribute::whereId($request->id)
                 ->where('restaurant_id', restaurant()->id)
                 ->where('meal_id',$request->meal_id)->delete();
         }
-        if($request->type == "option"){
+        if(request()->type == "option"){
             MealAttributeOption::whereId($request->id)
                 ->where('meal_id',$request->meal_id)
                 ->delete();
