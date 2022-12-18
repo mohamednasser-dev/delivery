bf3ec4830fd6dbe6813334a846153286be399d98
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-18 20:09:19 +0200

    home api

--- app/Http/Controllers/Api/Customer/HomeController.php
+++ app/Http/Controllers/Api/Customer/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
         $response = [
             'offers' => isset($offers) ? OfferResources::collection($offers) : [],
             'sections' => isset($sections) ?  SectionResources::collection($sections) : [],
-            'restaurants' => isset($restaurants) ? RestaurantResources::collection($restaurants) : [],
+            'restaurants' => isset($restaurants) ? RestaurantResources::collection($restaurants)->response()->getData(true) : [],
         ];
         return $this->sendSuccessData(__('lang.data_show_successfully'), $response);
     }
