975b6bb6a5f4c9b5c5ca856dc3351fbb20c14039
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-25 23:22:02 +0200

    meal details customer

--- app/Http/Controllers/Api/Customer/RestaurantController.php
+++ app/Http/Controllers/Api/Customer/RestaurantController.php
@@ -6,10 +6,18 @@ use App\Http\Resources\Customer\CustomerMealResources;
 use App\Http\Requests\Customer\MealDetailsRequest;
 use App\Http\Controllers\Controller;
 use App\Models\Meal;
+use Illuminate\Support\Facades\Request;
 
 
 class RestaurantController extends Controller
 {
+    public function restaurant_menu(Request $request)
+    {
+//        $data = $request->validated();
+//        $data = Meal::accepted()->active()->findOrFail($data['meal_id']);
+//        $data =  new CustomerMealResources($data);
+//        return $this->sendSuccessData(__('lang.data_show_successfully'), $data);
+    }
 
     public function meal_details(MealDetailsRequest $request)
     {
