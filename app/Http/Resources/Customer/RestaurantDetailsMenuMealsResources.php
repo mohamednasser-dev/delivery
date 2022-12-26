f8be97dcc4264bb39ecce03a3350873961e1d73f
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-26 22:51:03 +0200

    desc

--- app/Http/Resources/Customer/RestaurantDetailsMenuMealsResources.php
+++ app/Http/Resources/Customer/RestaurantDetailsMenuMealsResources.php
@@ -21,6 +21,7 @@ class RestaurantDetailsMenuMealsResources extends JsonResource
             'cover'=>$this->cover,
             'rate' => (double)4.0,
             'name'=> $lang == 'en' ? $this->name_en : $this->name_ar,
+            'description'=> $lang == 'en' ? "وصف" : "description",
             'latitude'=>$this->latitude,
             'longitude'=>$this->longitude,
             'address'=> !empty($this->address) ? $this->address : "",
