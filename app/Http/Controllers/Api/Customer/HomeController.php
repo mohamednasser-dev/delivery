b32f783aad445e999d228761ff86bc0930160f08
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-26 19:36:26 +0200

    restaurants

--- app/Http/Controllers/Api/Customer/HomeController.php
+++ app/Http/Controllers/Api/Customer/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
         $section_id = (array)$request->section_id;
         $offers = Offer::get();
         $sections = Section::select('id','name_'.$lang .' as title','image')->get()->makeHidden('name')->toArray();
-        $sestaurantsSections = isset($section_id) ? RestaurantSection::whereIn('section_id',$section_id)->pluck('restaurant_id') : Restaurant::pluck('id');
+        $restaurantsSections = isset($section_id) ? RestaurantSection::whereIn('section_id',$section_id)->pluck('restaurant_id') : Restaurant::pluck('id');
        // add all section in sections ....
         $all = [
             'id' => 0,
@@ -30,7 +30,8 @@ class HomeController extends Controller
             'image' => "",
         ];
         array_unshift($sections, $all);
-        $restaurants = Restaurant::whereIn('id',$sestaurantsSections)->paginate(pagination_number());
+        $restaurants = Restaurant::whereIn('id',$restaurantsSections)->paginate(pagination_number());
+        dd(sizeof($restaurants));
         $response = [
             'offers' => isset($offers) ? OfferResources::collection($offers) : [],
             'sections' => isset($sections) ? $sections  : [],
