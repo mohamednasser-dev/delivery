e57b096b5ce7dc879f1f8a16e141dd45c7d71861
Andrew Akram Albert <andrewalbert93501@gmail.com>
2022-12-29 19:58:45 +0200

    social login

--- database/migrations/2022_12_01_224109_create_customers_table.php
+++ database/migrations/2022_12_01_224109_create_customers_table.php
@@ -16,9 +16,9 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
             $table->string('password')->nullable();
-            $table->string('phone')->unique();
+            $table->string('phone')->nullable()->unique();
             $table->tinyInteger('active')->default(0);
             $table->tinyInteger('suspend')->default(0);
             $table->tinyInteger('notification')->default(1);
